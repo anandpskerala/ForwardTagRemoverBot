f7751753ffeb6f0315e008e5c16ac390e940c9f0
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-08-18 21:26:25 +0530

    Test

--- functions/functions.go
+++ functions/functions.go
@@ -22,3 +22,51 @@ func ForwardText(b ext.Bot, u *gotgbot.Update) error {
 	}
 	return err
 }
+
+func ForwardVideo(b ext.Bot, u *gotgbot.Update) error {
+	_, err := u.EffectiveMessage.ReplyVideo(b.NewFileId(u.EffectiveMessage.Video.FileId))
+	if err != nil {
+		b.Logger.Errorf("Error in sending video", zap.Error(err))
+	}
+	return err
+}
+
+func ForwardDocument(b ext.Bot, u *gotgbot.Update) error {
+	_, err := u.EffectiveMessage.ReplyDocument(b.NewFileId(u.EffectiveMessage.Document.FileId))
+	if err != nil {
+		b.Logger.Errorf("Error in sending document", zap.Error(err))
+	}
+	return err
+}
+
+func ForwardPhoto(b ext.Bot, u *gotgbot.Update) error {
+	_, err := u.EffectiveMessage.ReplyPhoto(b.NewFileId(u.EffectiveMessage.Photo[len(u.EffectiveMessage.Photo)-1].FileId))
+	if err != nil {
+		b.Logger.Errorf("Error in sending photo", zap.Error(err))
+	}
+	return err
+}
+
+func ForwardSticker(b ext.Bot, u *gotgbot.Update) error {
+	_, err := u.EffectiveMessage.ReplySticker(b.NewFileId(u.EffectiveMessage.Sticker.FileId))
+	if err != nil {
+		b.Logger.Errorf("Error in sending sticker", zap.Error(err))
+	}
+	return err
+}
+
+func ForwardAudio(b ext.Bot, u *gotgbot.Update) error {
+	_, err := u.EffectiveMessage.ReplyAudio(b.NewFileId(u.EffectiveMessage.Audio.FileId))
+	if err != nil {
+		b.Logger.Errorf("Error in sending Audio", zap.Error(err))
+	}
+	return err
+}
+
+func ForwardVoice(b ext.Bot, u *gotgbot.Update) error {
+	_, err := u.EffectiveMessage.ReplyVoice(b.NewFileId(u.EffectiveMessage.Voice.FileId))
+	if err != nil {
+		b.Logger.Errorf("Error in sending voice", zap.Error(err))
+	}
+	return err
+}
